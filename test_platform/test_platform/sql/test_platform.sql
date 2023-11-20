13ae3a03bc86defe65b98767545e24f3ca138b25
Annona <2480916765>
2023-11-20 19:52:07 +0800

    20231116：1、提交qq登录视图内容

--- test_platform/test_platform/sql/test_platform.sql
+++ test_platform/test_platform/sql/test_platform.sql
@@ -7,3 +7,8 @@
 # grant all on test_platform.* to 'dev_user'@'%';
 # 刷新权限
 # flush privileges ;
+# show columns from tb_user;
+# show status ;
+# SHOW ERRORS;
+# SHOW WARNINGS;
+select  distinct  *from tb_user;
